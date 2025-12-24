7eecde021338759272e7d56901f95b1b95f8ae81
Jamiyanmyadag <batmonhjaagii@gamil.com>
2025-12-24 15:34:14 +0530

    byod3 brm

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = var.aws_region  
 }
 
-resource "aws_instance" "splunk_server" 
+resource "aws_instance" "splunk_server" {
   ami           = "ami-012967cc5a8c9f891" 
   instance_type = "t2.micro"
   subnet_id     = var.subnet_id
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
-  description = "AWS region"
   type        = string
+  default = "us-east-1"
 }
 
 variable "vpc_id" {
